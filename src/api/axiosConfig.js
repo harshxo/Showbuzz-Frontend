817b9b35a185079fd60ac9798cf6dd97ee643b27
harshxo <shrivastavaharsh876@gmail.com>
2024-08-22 22:55:31 +0530

    added backend url to apiconfig

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({ 
-    baseURL: 'http://localhost:8080/',
+    // baseURL: 'http://localhost:8080/',
+    baseURL: 'cooperative-clarity-production.up.railway.app',
+
     headers: {
         "ngrok-skip-browser-warning": "true"   
     },
